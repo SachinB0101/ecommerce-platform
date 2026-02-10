a8768e1f1cf4c3043e3830f03d0adb253664190e
SachinB <sachin1214bhatt@gmail.com>
2026-02-10 15:16:08 -0600

    Add logo in the Logo.tsx file
    We use Button component to make it look like a button and used Link to address to a link

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,6 +1,14 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { MdStore } from "react-icons/md";
+
 const Logo = () => {
   return (
-    <div>Logo</div>
-  )
-}
-export default Logo
+    <Button size='icon' asChild>
+      <Link href="/">
+        <MdStore className="w-6 h-6"></MdStore>
+      </Link>
+    </Button>
+  );
+};
+export default Logo;
