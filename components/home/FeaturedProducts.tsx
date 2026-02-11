d0a2504573597aab6e45542cc63e00a1d1d5cbdf
SachinB <sachin1214bhatt@gmail.com>
2026-02-11 01:00:38 -0600

    Create Loading container

--- components/home/FeaturedProducts.tsx
+++ components/home/FeaturedProducts.tsx
@@ -3,6 +3,7 @@ import EmptyList from "../global/EmptyList";
 import SectionTitle from "../global/SectionTitle";
 import ProductsGrid from "../products/ProductsGrid";
 
+
 async function FeaturedProducts() {
   const products = await fetchFeaturedProducts();
   if (products.length === 0) return <EmptyList />;
