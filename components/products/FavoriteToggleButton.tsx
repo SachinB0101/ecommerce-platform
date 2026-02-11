41cc0a11374fe5b54b670ea5d8d0725fb29dc4d8
SachinB <sachin1214bhatt@gmail.com>
2026-02-10 23:13:00 -0600

    create a featch product function and create favorite toggle button icon

--- components/products/FavoriteToggleButton.tsx
+++ components/products/FavoriteToggleButton.tsx
@@ -1,6 +1,9 @@
-const FavoriteToggleButton = () => {
-  return (
-    <div>FavoriteToggleButton</div>
-  )
-}
-export default FavoriteToggleButton
+import { FaHeart } from "react-icons/fa";
+import { Button } from "../ui/button";
+
+const FavoriteToggleButton = ({ productId }: { productId: string }) => {
+  return <Button size='icon' variant='outline' className="p-2 cursor-pointer">
+    <FaHeart/>
+  </Button>
+};
+export default FavoriteToggleButton;
