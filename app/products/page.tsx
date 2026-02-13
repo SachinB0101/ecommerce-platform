3950188a15363ad69c48fac37dcda119d7a726f7
SachinB <sachin1214bhatt@gmail.com>
2026-02-13 17:49:46 -0600

    Make the product page

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,6 +1,14 @@
-const ProductsPage = () => {
-  return (
-    <div>ProductsPage</div>
-  )
-}
-export default ProductsPage
+import ProductsContainer from "@/components/products/ProductsContainer";
+
+const ProductsPage = ({
+  searchParams,
+}: {
+  searchParams: { layout?: string; search?: string };
+}) => {
+  //searchParams: A special props we have access to in a server component
+  const layout = searchParams.layout || "grid";
+  const search = searchParams.search || "";
+
+  return <ProductsContainer layout={layout} search={search} />;
+};
+export default ProductsPage;
